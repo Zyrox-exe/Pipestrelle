15b71631f11dfbe3fc5d5515b77afbc60210a41e
zyrox <183991423+Zyrox-exe@users.noreply.github.com>
2026-07-08 18:15:40 +0530

    Completed Buttons Logic

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const sleepBtn = document.getElementById("sleepBtn");
 const happinessBar = document.getElementById("happinessBar");
 const playBtn = document.getElementById("playBtn");
 
+// Bars
 setInterval(function() {
     hunger = hunger - 0.9;
     energy = energy - 0.5;
@@ -20,10 +21,42 @@ setInterval(function() {
     energyBar.value = energy;
     happinessBar.value = happiness;
 
-    if(hunger <= 0 || happiness <= 0){
+    if(hunger <= 0){
         alert("Oh no! Pip ran away to find food. Feed him better next time!!!");
         hunger = 100;
         energy = 100;
         happiness = 100;
+    } else if(happiness <= 0){
+        alert("Pip grew bored of you, so it left for more fun things. Play with him more next time!!!");
+        hunger = 100;
+        energy = 100;
+        happiness = 100;
+    }
+    else if(energy <= 0){
+        alert("Pip is sleeping, give it some rest!!!");
     }
 }, 1000);
+
+// Buttons
+feedBtn.addEventListener('click', function(){
+    if (hunger < 100){
+        hunger += 15;
+        if(hunger > 100) hunger = 100;
+        hungerBar.value = hunger;
+    }
+});
+
+sleepBtn.addEventListener('click', function(){
+    if (energy < 100){
+        energy += 20;
+        if(energy > 100) energy = 100;
+        energyBar.value = energy;
+    }
+playBtn.addEventListener('click', function(){
+    if (happiness < 100){
+        happiness += 10;
+        if(happiness > 100) happiness = 100;
+        happinessBar.value = happiness;
+    }
+})
+})
